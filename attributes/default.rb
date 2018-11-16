9737bb80bf2ff69fe223a4db8af7d0a2cfa00e72
Matt Van <m@mattvv.com>
2018-11-16 12:27:05 -0800

    hardcode new url

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,5 @@ else
 	default[:wkhtmltopdf][:arch] = 'i386'
 end
 
-default[:wkhtmltopdf][:static_download_url] = "https://github.com/wkhtmltopdf/obsolete-downloads/releases/download/linux/wkhtmltopdf-#{node[:wkhtmltopdf][:version]}-static-#{node[:wkhtmltopdf][:arch]}.tar.bz2"
-default[:wkhtmltopdf][:binary_name] = "wkhtmltopdf-#{node[:wkhtmltopdf][:arch]}"
+default[:wkhtmltopdf][:static_download_url] = "https://github.com/wkhtmltopdf/obsolete-downloads/releases/download/linux/wkhtmltopdf-0.9.9-static-amd64.tar.bz2"
+default[:wkhtmltopdf][:binary_name] = "wkhtmltopdf-0.9.9-static-amd64.tar.bz2"
