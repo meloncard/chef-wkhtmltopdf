c80341580c28169088d21b9e3ebf8f34ef5e8b4c
Matt Van <m@mattvv.com>
2018-11-16 12:16:33 -0800

    Update URL

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,5 @@ else
 	default[:wkhtmltopdf][:arch] = 'i386'
 end
 
-default[:wkhtmltopdf][:static_download_url] = "http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-#{node[:wkhtmltopdf][:version]}-static-#{node[:wkhtmltopdf][:arch]}.tar.bz2"
+default[:wkhtmltopdf][:static_download_url] = "https://github.com/wkhtmltopdf/obsolete-downloads/releases/download/linux/wkhtmltopdf-#{node[:wkhtmltopdf][:version]}-static-#{node[:wkhtmltopdf][:arch]}.tar.bz2"
 default[:wkhtmltopdf][:binary_name] = "wkhtmltopdf-#{node[:wkhtmltopdf][:arch]}"
